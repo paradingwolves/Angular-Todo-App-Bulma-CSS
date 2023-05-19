d2fc94e41378b541ff2276498e70f3f93e276cc9
Justin Brierley <91489058+paradingwolves@users.noreply.github.com>
2023-05-19 13:16:48 -0400

    Added a Search Filter
    Users can now search for specific notes

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
 import { trigger, transition, style, animate, query, stagger } from '@angular/animations';
+import { all } from 'axios';
 
 @Component({
   selector: 'app-notes-list',
@@ -80,15 +81,65 @@ import { trigger, transition, style, animate, query, stagger } from '@angular/an
 export class NotesListComponent implements OnInit {
 
   notes: Note[] = new Array<Note>();
+  filteredNotes: Note[] = new Array<Note>();
 
   constructor(private notesService: NotesService) {}
 
   ngOnInit(): void {
     // retrieve all notes from NotesService
     this.notes = this.notesService.getAll();
+    this.filteredNotes = this.notes;
   }
 
   deleteNote(id: number) {
     this.notesService.delete(id);
+  }
+
+  filter(query: string | null) {
+    if (query !== null) {
+      query = query.toLowerCase().trim();
+
+      let allResults: Note[] = new Array<Note>();
+      // split search query to individual words
+      let terms: string[] = query.split(' '); // split on spaces
+      // remove duplicate search terms
+      terms = this.removeDuplicates(terms);
+      // compile all relevant results into the allResults array
+      terms.forEach(term => {
+        let results: Note[] = this.relevantNotes(term);
+        // append results to allResults array
+        allResults = [...allResults, ...results];
+      });
+
+      // all results will include duplicate notes
+      // because a particular note can be the result of many search terms
+      // we do not want to show the same note multiple times so we must remove dupes
+      let uniqueResults = this.removeDuplicates(allResults);
+      this.filteredNotes = uniqueResults;
+    }
+  }
+
+
+  removeDuplicates(arr:Array<any>) : Array<any> {
+    let uniqueResults: Set<any> = new Set<any>();
+    // loop through the input array and add items to the set
+    arr.forEach(e => uniqueResults.add(e));
+
+    return Array.from(uniqueResults);
+  }
+
+  relevantNotes(query: string) : Array<Note> {
+    query = query.toLowerCase().trim();
+    let relevantNotes = this.notes.filter(note => {
+      if ( note.title && note.title.toLowerCase().includes(query)) {
+        return true;
+      }
+      if (note.body && note.body.toLowerCase().includes(query)) {
+        return true;
+      }
+      return false;
+    })
+
+    return relevantNotes;
   }
 }
