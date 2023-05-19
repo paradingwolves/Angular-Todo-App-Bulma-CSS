fb492c46eb0723a2c88087f42daaa95941b3996f
Justin Brierley <91489058+paradingwolves@users.noreply.github.com>
2023-05-19 12:15:36 -0400

    Adding Animations to Note Entries

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,11 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-notes-list',
   templateUrl: './notes-list.component.html',
-  styleUrls: ['./notes-list.component.scss']
+  styleUrls: ['./notes-list.component.scss'],
+  animations: [
+    trigger('itemAnim', [
+      // entry animation
+      transition('void => *', [
+        // set initial state
+        style({
+          height: 0,
+          opacity: 0,
+          transform: 'scale(0.85)',
+          'margin-bottom': 0,
+
+          // we have to expand out padding properties
+          paddingTop: 0,
+          paddingBottom: 0,
+          paddingLeft: 0,
+          paddingRight: 0
+        }),
+        // we want to animate the spacing (includes height and margin)
+        animate('50ms', style({
+          height: '*',
+          'margin-bottom': '*',
+          paddingTop: '*',
+          paddingBottom: '*',
+          paddingLeft: '*',
+          paddingRight: '*'
+        })),
+        animate(68)
+      ])
+    ])
+  ]
 })
 export class NotesListComponent implements OnInit {
 
